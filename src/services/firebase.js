f9c269298c3b1c33b80502816cf1eafb5633bc68
Ian J Sikes <ianjsikes@gmail.com>
2021-05-19 18:13:57 -0700

    Add react-scripts

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,7 +2,7 @@ import { FirebaseApp } from 'firebase-rxjs'
 
 export const setupFirebase = () => {
   const app = new FirebaseApp({
-    options: JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG)
+    options: JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG),
   })
   return app.database()
 }
